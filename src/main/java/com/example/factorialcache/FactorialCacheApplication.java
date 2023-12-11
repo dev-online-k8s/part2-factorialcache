b33fbb4411235f9090d4b002d6084549c430deaa
June Heo <papercut@gmail.com>
2023-12-11 22:12:46 +0900

    initial project

---
+++ src/main/java/com/example/factorialcache/FactorialCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.factorialcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactorialCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactorialCacheApplication.class, args);
+	}
+
+}
